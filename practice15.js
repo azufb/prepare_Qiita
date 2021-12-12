b50c178e6c6e12d218f90ee88df16725e9c9bda9
azufb <curiousazunas@gmail.com>
2021-12-12 13:54:01 +0900

    記事

---
+++ practice15.js
@@ -0,0 +1,10 @@
+let message = 'Merry Christmas and a Happy New Year!';
+
+console.log(message.slice());
+console.log(message.slice(20));
+console.log(message.slice(-9));
+
+console.log(message.slice(6, 14));
+console.log(message.slice(-21, -10));
+
+console.log(message.slice(27));
