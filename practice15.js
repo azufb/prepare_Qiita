9687f617f52ec478274ea9aeb3d21937f1bbca2f
azufb <curiousazunas@gmail.com>
2021-12-12 17:56:36 +0900

    文字へのアクセス検証

--- practice15.js
+++ practice15.js
@@ -8,3 +8,11 @@ console.log(message.slice(6, 14));
 console.log(message.slice(-21, -10));
 
 console.log(message.slice(27));
+
+let sampleStr = 'Hello!';
+console.log(sampleStr[0]);
+console.log(sampleStr[1]);
+console.log(sampleStr[2]);
+console.log(sampleStr[3]);
+console.log(sampleStr[4]);
+console.log(sampleStr[5]);
